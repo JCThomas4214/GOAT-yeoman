c3782e349f47a62ab6f52db4e1a42fb4892b5828
JT <jc.thomas4214@gmail.com>
2016-11-10 20:57:37 -0800

    create app default command

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,27 @@
-var generators = require('yeoman-generator');
+var generators = require('yeoman-generator'),
+	_ = require('lodash'),
+	fs = require('graceful-fs');
 
 module.exports = generators.Base.extend({
-  method1: function () {
-    console.log('method 1 just ran');
+  // note: arguments and options should be defined in the constructor.
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+    // This makes `appname` a required argument.
+    this.argument('appname', { type: String, required: false, default: 'GOAT-stack' });
+    console.log('The app is called ' + this.appname);
+
+
   },
-  method2: function () {
-    console.log('method 2 just ran');
-  }
+  // Writes the application to the name of the project
+  writing: function () {
+  	this.sourceRoot('GOAT-yeoman/templates');
+    this.fs.copyTpl(
+      this.templatePath('app'),
+      this.destinationPath(this.appname)
+    );
+  },
+  // Starts npm install
+  // installNpm: function() {
+  //   this.npmInstall();
+  // }
 });
