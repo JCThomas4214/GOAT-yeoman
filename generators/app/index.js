b003ef9e71cdc537e969f40103b27e13056a1f5f
CHRISTOPHER HAUGEN <chaugen24@knights.ucf.edu>
2017-11-06 17:31:10 -0500

    prompts(docs) index all related docs for the GOATstack

--- generators/app/index.js
+++ generators/app/index.js
@@ -45,7 +45,7 @@ module.exports = class extends Generator {
         { name: `Generate a new app`, value: 'newApp' },
         { name: 'Add boilerplate to an existing app', value: 'boilerplate' },
         { type: 'separator', line: '----------- Resources --------------------' },
-        { name: 'View Documentation', value: 'docs' },
+        { name: 'View Documentation', value: 'viewDocs' },
         { name: 'View Demo App', value: 'demo' },
         { type: 'separator', line: '----------- Get involved -----------------' },
         { name: 'Star us on Github!', value: 'star' },
@@ -63,7 +63,7 @@ module.exports = class extends Generator {
       type: 'checkbox',
       name: 'databases',
       message: 'Select what databases you would like to use.' + chalk.yellow.bold('\n\n  **If no databases are selected the generated stack will be a dbless solution**\n'),
-      choices: ['MongoDB', 'Apache Cassandra', 'PostgresSQL', 'MySQL', 'MariaDB', 'SQLite', 'MSSQL'],
+      choices: ['MongoDB', 'Apache Cassandra', 'PostgreSQL', 'MySQL', 'MariaDB', 'SQLite', 'MSSQL'],
       when: res => res.welcome === 'newApp'
     }, {
       type: 'confirm',
@@ -118,13 +118,63 @@ module.exports = class extends Generator {
       when: res => res.welcome === 'newApp' && res.analyticschoice
     },
 
-    //boilerplate prompts
+    //View Documentation prompts
     {
-      type: 'list',
-      name: 'subgens',
-      message: 'Select the template you would like to generate',
-      choices: ['module', 'submodule', 'component', 'service', 'directive', 'pipe', 'actions', 'store-item', 'endpoint', 'help!'],
-      when: res => res.welcome === 'boilerplate'
+      type: 'checkbox',
+      name: 'docs',
+      message: 'Select the Documentation you would like to navigate to',
+      choices: [
+        {name: 'GOATstack - Yeoman generator', value: 'goatyeoman'},
+        {name: 'GOATstack - Source', value: 'goatstack'},
+        {name: 'Third Party Docs', value: '3rd'}
+      ],
+      when: res => res.welcome === 'viewDocs'
+    },
+    {
+      type: 'checkbox',
+      name: 'thirdParty',
+      message: 'Select the third party Documentation you would like to navigate to',
+      choices: [
+
+        { type: 'separator', line: '----------- Languages --------------------' },
+        {name: 'TypeScript', value: 'ts'},
+        {name: 'JavaScript', value: 'js'},
+
+        { type: 'separator', line: '----------- Build Tools --------------------' },
+        {name: 'npm', value: 'npm'},
+        {name: 'Gulp', value: 'gulp'},
+
+        { type: 'separator', line: '----------- Front End --------------------' },
+        {name: 'Angular', value: 'ng'},
+        {name: 'Redux', value: 'redux'},
+        {name: 'RxJs', value: 'rx'},
+        {name: 'Immutable', value: 'immutable'},
+        {name: 'SocketIO', value: 'socket'},
+
+        { type: 'separator', line: '----------- Servers --------------------' },
+        {name: 'NodeJS', value: 'node'},
+        {name: 'Express.js', value: 'express'},
+        {name: 'SocketIO', value: 'socket'},
+        {name: 'Mongoose', value: 'mongoose'},
+        {name: 'Sequelize', value: 'sequelize'},
+        {name: 'Passport', value: 'pass'},
+        {name: 'Cassandra Driver', value: 'cassdriver'},
+
+        { type: 'separator', line: '----------- Databases --------------------' },
+        {name: 'MongoDB', value: 'mongo'},
+        {name: 'Apache Cassandra', value: 'cass'},
+        {name: 'PostgreSQL', value: 'postgre'},
+        {name: 'MySQL', value: 'mysql'},
+        {name: 'MariaDB', value: 'maria'},
+        {name: 'SQLite', value: 'sqlite'},
+        {name: 'MSSQL', value: 'mssql'},
+
+        { type: 'separator', line: '----------- Testing Tools --------------------' },
+        {name: 'Jasmine', value: 'jasmine'},
+        {name: 'Karma', value: 'karma'},
+        {name: 'Protractor', value: 'protractor'}
+      ],
+      when: res => res.docs.indexOf('3rd') > -1
     }
 
     ]).then(function (answers) {
@@ -166,8 +216,54 @@ module.exports = class extends Generator {
             return this.log('help selected');
 
         }
-      } else if (answers.welcome === 'docs') {
-        exec('explorer "https://github.com/JCThomas4214/GOAT-yeoman"');
+      } else if (answers.welcome === 'viewDocs') {
+
+        const goatyeoman = "https://github.com/JCThomas4214/GOAT-yeoman";
+        const goatstack = "https://github.com/projectSHAI/GOATstack/wiki";
+        for(let i = 0; i <= answers.docs.length; i++) {
+
+          if(answers.docs[i] === '3rd') {
+
+            const ts = "https://www.typescriptlang.org/docs/home.html",
+                  js = "https://developer.mozilla.org/en-US/docs/Web/javascript",
+                  npm = "https://docs.npmjs.com/",
+                  gulp = "https://github.com/gulpjs/gulp/blob/master/docs/API.md",
+                  ng = "https://angular.io/docs/",
+                  redux = "https://redux.js.org/",
+                  rx = "http://reactivex.io/rxjs/",
+                  immutable = "https://facebook.github.io/immutable-js/docs/#/",
+                  socket = "https://socket.io/docs/#",
+                  node = "https://nodejs.org/en/docs/",
+                  express = "http://expressjs.com/",
+                  mongoose = "http://mongoosejs.com/docs/api.html",
+                  sequelize = "http://docs.sequelizejs.com/manual/installation/getting-started.html",
+                  pass = "http://www.passportjs.org/docs",
+                  cassdriver = "https://github.com/datastax/nodejs-driver",
+                  mongo = "https://docs.mongodb.com/",
+                  cass = "http://cassandra.apache.org/doc/latest/",
+                  postgre = "https://www.postgresql.org/docs/",
+                  mysql = "https://dev.mysql.com/doc/",
+                  maria = "https://mariadb.org/learn/",
+                  sqlite = "https://sqlite.org/docs.html",
+                  mssql = "https://docs.microsoft.com/en-us/sql/sql-server/sql-server-technical-documentation",
+                  jasmine = "https://jasmine.github.io/",
+                  karma = "http://karma-runner.github.io/0.13/index.html",
+                  protractor = "http://www.protractortest.org/#/";
+
+            for(let i = 0; i <= answers.thirdParty.length; i++) {
+              this.log('Navigating to inner' + chalk.blue(answers.thirdParty[i]));
+              this.log(answers.thirdParty);
+              exec('explorer ' + answers.thirdParty[i]);
+            }
+
+          } else {
+            this.log('Navigating to outer' + chalk.blue(answers.docs[i]));
+            this.log(answers.docs);
+            exec('explorer ' + answers.docs[i]);
+          }
+
+        }
+
       } else if (answers.welcome === 'demo') {
 
 
@@ -186,7 +282,7 @@ module.exports = class extends Generator {
       } else if (answers.welcome === 'jt') {
 
 
-      } else if (answers.welcome === 'docs') {
+      } else if (answers.welcome === 'viewD') {
 
 
       } else if (answers.welcome === 'hire') {
@@ -200,14 +296,14 @@ module.exports = class extends Generator {
   // Where you write the generator specific files (routes, controllers, etc)
   writing() {
     if (this.appname) {
-      newApp.writing(this, glob);
+      // newApp.writing(this, glob);
     }
   }
 
   // Where installations are run (npm, bower)
   install() {
     if (this.appname) {
-      newApp.installNpm(this);
+      // newApp.installNpm(this);
     }
   }
 
